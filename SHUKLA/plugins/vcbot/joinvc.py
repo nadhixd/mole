1e2726825eab66cff8355f3f9eece56c611f51a0
nadhixd <raam43927@gmail.com>
2025-02-03 15:27:07 +0530

    Update joinvc.py

--- SHUKLA/plugins/vcbot/joinvc.py
+++ SHUKLA/plugins/vcbot/joinvc.py
@@ -1,25 +1,33 @@
 from ... import *
 from ...modules.mongo.streams import *
 from pyrogram import filters
-from pytgcalls.exceptions import GroupCallNotFound
+from pytgcalls.exceptions import NoActiveGroupCall
 
 
 @app.on_message(cdx(["join", "joinvc"]) & ~filters.private)
 @sudo_users_only
 async def join_vc(client, message):
     chat_id = message.chat.id
-    try:
-        a = await call.get_call(chat_id)
-        if (a.status == "not_playing"
-            or a.status == "playing"
-            or a.status == "paused"
-        ):
-            await eor(message, "**Already Joined!**")
-    except GroupCallNotFound:
-        await call.join_group_call(chat_id)
-        await eor(message, "**Joined VC!**")
-    except Exception as e:
-        print(f"Error: {e}")
+    a_calls = await call.calls
+    if_chat = a_calls.get(chat_id)
+    if if_chat:
+        return await eor(
+            message, "**𝒎𝒂𝒊 𝒕𝒐 𝒗𝒄 𝒑𝒆 𝒉𝒊 𝒉𝒖 𝒌𝒊𝒔𝒌𝒐 𝒃𝒖𝒍𝒂 𝒓𝒂𝒉𝒆 !**"
+        )
+    if not if_chat:
+        try:
+            await call.play(chat_id)
+            return await eor(
+                message, "**𝒂𝒂 𝒈𝒂𝒚𝒆 𝒃𝒐𝒍𝒊𝒚𝒆 𝒂𝒃**"
+            )
+        except NoActiveGroupCall:
+            return await eor(
+                message, "**𝒃𝒄 𝒗𝒄 𝒃𝒂𝒏𝒅 𝒉𝒂𝒊!**"
+            )
+        except Exception as e:
+            print(f"Error: {e}")
+            pass
+        
 
 
 @app.on_message(cdz(["cjoin", "cjoinvc"]))
@@ -31,15 +39,22 @@ async def join_vc_(client, message):
         return await eor(message,
             "**🥀 No Stream Chat Set❗**"
     )
-    try:
-        a = await call.get_call(chat_id)
-        if (a.status == "not_playing"
-            or a.status == "playing"
-            or a.status == "paused"
-        ):
-            await eor(message, "**Already Joined!**")
-    except GroupCallNotFound:
-        await call.join_group_call(chat_id)
-        await eor(message, "**Joined VC!**")
-    except Exception as e:
-        print(f"Error: {e}")
+    a_calls = await call.calls
+    if_chat = a_calls.get(chat_id)
+    if if_chat:
+        return await eor(
+            message, "**𝒎𝒂𝒊 𝒕𝒐 𝒗𝒄 𝒑𝒆 𝒉𝒊 𝒉𝒖 𝒌𝒊𝒔𝒌𝒐 𝒃𝒖𝒍𝒂 𝒓𝒂𝒉𝒆 !**"
+        )
+    if not if_chat:
+        try:
+            await call.play(chat_id)
+            return await eor(
+                message, "**𝒂𝒂 𝒈𝒂𝒚𝒆 𝒃𝒐𝒍𝒊𝒚𝒆 𝒋𝒊 !**"
+            )
+        except NoActiveGroupCall:
+            return await eor(
+                message, "**𝒃𝒄 𝒗𝒄 𝒃𝒂𝒏𝒅 𝒌𝒂𝒓 𝒅𝒊𝒚𝒂!**"
+            )
+        except Exception as e:
+            print(f"Error: {e}")
+            pass
